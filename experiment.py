b2de022a3d35f8f4aad07a1850340da2df71b4a4
전해주 <haejoo@jeonhaejuui-MacBookAir.local>
2021-05-20 18:55:22 +0900

    Update experiments.py

--- experiment.py
+++ experiment.py
@@ -191,14 +191,12 @@ if __name__ == '__main__' :
             s2, m2 = check_count(dag, base_small, acceptance, deadline, cpu_num, False)
             s3, m3 = check_count(dag, base_middle, acceptance, deadline, cpu_num, False)
             s4, m4 = check_count(dag, base_large, acceptance, deadline, cpu_num, False)
-            if m0 == 1 or m1 == 1 :
-                continue
 
             score[0] += s0 ; miss[0] += m0 ; critical[0] += 1 if s0==1 and m0==1 else 0
             score[1] += s1 ; miss[1] += m1 ; critical[1] += 1 if s1==1 and m1==1 else 0
-            score[2] += s2 ; miss[2] += m2 ; critical[2] += 1 if s2==1 and m2==1 else 0
-            score[3] += s3 ; miss[3] += m3 ; critical[3] += 1 if s3==1 and m3==1 else 0
-            score[4] += s4 ; miss[4] += m4 ; critical[4] += 1 if s4==1 and m4==1 else 0
+            score[2] += s2 ; miss[2] += m2 ; critical[2] += 1 if s2==1 or m2==1 else 0
+            score[3] += s3 ; miss[3] += m3 ; critical[3] += 1 if s3==1 or m3==1 else 0
+            score[4] += s4 ; miss[4] += m4 ; critical[4] += 1 if s4==1 or m4==1 else 0
 
             f.write("{},{},{},{},{},{},{},{},{},{}\n".format(s0, m0, s1, m1, s2, m2, s3, m3, s4, m4))
 
@@ -213,7 +211,6 @@ if __name__ == '__main__' :
     ## sum up all result
     print("Error: ", continued)
     print("Unacceptable: ", score)
-    print("miss: ", miss)
     print("critical: ", critical)
     # f.write("{},{},{},{},{}".format(*score))
     # f.write("{},{},{},{},{}".format(*miss))
